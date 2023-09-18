d840253a3844556c0ca7f92933e5f9e746c75226
Kender Jose Zambrano Gorrin <kender.zambrano@santander.cl>
2023-09-18 18:06:14 -0300

    Fix en versiculos y oraciones

--- src/app/pages/verse/verse.component.ts
+++ src/app/pages/verse/verse.component.ts
@@ -13,17 +13,45 @@ export class VerseComponent implements OnInit {
       description: 'El Señor es mi pastor; nada me faltará.',
     },
     {
-      title: 'Proverbios 3:5-6',
-      description: 'Confía en el Señor de todo corazón, y no en tu propia inteligencia. Reconócelo en todos tus caminos, y él allanará tus sendas.',
+      title: 'Filipenses 4:13',
+      description: 'Todo lo puedo en Cristo que me fortalece.'
     },
     {
       title: 'Isaías 41:10',
-      description: 'No temas, porque yo estoy contigo; no desmayes, porque yo soy tu Dios que te fortalece; siempre te ayudaré, siempre te sustentaré con la diestra de mi justicia.',
+      description: 'No temas, porque yo estoy contigo; no desmayes, porque yo soy tu Dios que te fortalezco; siempre te ayudaré, siempre te sustentaré con la diestra de mi justicia.'
+    },
+    {
+      title: 'Josué 1:9',
+      description: 'Mira que te mando que te esfuerces y seas valiente; no temas ni desmayes, porque Jehová tu Dios estará contigo en dondequiera que vayas.'
+    },
+    {
+      title: 'Romanos 8:28',
+      description: 'Y sabemos que a los que aman a Dios, todas las cosas les ayudan a bien, esto es, a los que conforme a su propósito son llamados.'
+    },
+    {
+      title: 'Salmo 34:17',
+      description: 'Claman los justos, y Jehová oye, y los libra de todas sus angustias.'
+    },
+    {
+      title: 'Mateo 11:28',
+      description: 'Venid a mí todos los que estáis trabajados y cargados, y yo os haré descansar.'
+    },
+    {
+      title: 'Isaías 40:31',
+      description: 'Pero los que esperan a Jehová tendrán nuevas fuerzas; levantarán alas como las águilas; correrán, y no se cansarán; caminarán, y no se fatigarán.'
     },
     {
       title: 'Jeremías 29:11',
-      description: 'Porque yo sé los planes que tengo para ustedes, dice el Señor, planes de bienestar y no de calamidad, para darles un futuro y una esperanza.',
+      description: 'Porque yo sé los pensamientos que tengo acerca de vosotros, dice Jehová, pensamientos de paz, y no de mal, para daros el fin que esperáis.'
     },
+    {
+      title: 'Salmos 46:1',
+      description: 'Dios es nuestro amparo y fortaleza, nuestro pronto auxilio en las tribulaciones.'
+    },
+    {
+      title: 'Proverbios 3:5-6',
+      description: 'Fíate de Jehová de todo tu corazón, y no te apoyes en tu propia prudencia. Reconócelo en todos tus caminos, y él enderezará tus veredas.'
+    }
   ];
 
   public todayVerse: any;
